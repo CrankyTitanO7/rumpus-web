98099f7264fed7bd1337ad4100848b5052c1b472
CrankyTitanO7 <nedajtheawesome@gmail.com>
2025-11-26 18:31:48 -0500

    added sidebar.js and components folder

---
+++ components/Sidebar.js
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li> <a href="#section1">Countdown to Latest Issue</a> </li>
+        <li> <a href="#section2">Latest Issue</a> </li>
+        <li> <a href="#section3">Past Issues</a> </li>
+      </ul>
+    </aside>
+  );
+}
